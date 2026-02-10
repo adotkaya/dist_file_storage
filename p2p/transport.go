aa012cc546a8908c8ead1a7da81e94dc1530364a
adotkaya <adotkaya@gmail.com>
2026-02-10 16:15:50 +0300

    Initial commit

---
+++ p2p/transport.go
@@ -0,0 +1,10 @@
+package p2p
+
+// Peer is remote node
+type Peer interface {
+}
+
+// Handles communication between nodes in the network.
+// Can be TCP, UDP, websockets etc...
+type Transport interface {
+}
